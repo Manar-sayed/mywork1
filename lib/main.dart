f9eaf0b66b40964f6c2a765c023d5a6fde59b921
Manar-sayed <manarsayed20702070@gmail.com>
2022-07-22 20:44:06 +0200

    add new work

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:mywork1/home.dart';
+import 'package:mywork1/work2/onboardingScreen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,7 +15,12 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-     home: home(),
+     title: 'Flutter Demo',
+     theme: ThemeData(
+       fontFamily: GoogleFonts.openSans().fontFamily,
+     ),
+
+     home: OndoardingScreen(),
     );
   }
 }
